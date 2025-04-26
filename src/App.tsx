ce3788258625c730df18a14d4fdde44cebbfb479
satellair <85502719+satellair@users.noreply.github.com>
2025-04-27 02:02:45 +0700

    🔧fix: checkbox type

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const roundRobinSongAmt = roundRobinList.length;
   const finalSongAmt = finalList.length;
 
-  const [isFinal, setIsFinal] = useState<boolean>(false);
+  const [isFinal, setIsFinal] = useState<number>(0);
   const [randomValue, setRandomValue] = useState(0);
   const [result, setResult] = useState<songData | undefined>();
   const [counter, setCounter] = useState(0);
@@ -70,7 +70,7 @@ function App() {
           <input
             type="checkbox"
             value={isFinal}
-            onChange={() => setIsFinal(!isFinal)}
+            onChange={() => setIsFinal(isFinal === 0 ? 1 : 0)}
             className="sr-only peer"
           />
           <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600" />
